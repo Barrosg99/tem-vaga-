3b6ef5a7dff6884897a2fb14045b61ed075cd9bb
KaydeSix <srsenhorial@gmail.com>
2022-05-27 20:42:21 -0300

    Adiciona pequena validação no login da tela login

--- scripts/TelaLogin.js
+++ scripts/TelaLogin.js
@@ -1,3 +1,8 @@
 function ValidarLogin() {
-    
+    login = document.getElementById("login").value
+    if (login.length < 5){
+        alert("O username tem no minimo 5 caracteres!")}
+    else{
+        window.location.href = "./TelaPesquisa.html";
+    }
 }
