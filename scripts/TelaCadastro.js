42771ea6eac2dc9e4e64ecd84d870b44bc3665e6
IDavidI7 <102183014+IDavidI7@users.noreply.github.com>
2022-05-31 19:00:55 -0300

    adciona JS

---
+++ scripts/TelaCadastro.js
@@ -0,0 +1,15 @@
+function ValidarCadastro() {
+    username = document.getElementById("username").value
+    email = document.getElementById("email").value
+    password = document.getElementById("password").value
+    passwordConfirmation = document.getElementById("passwordConfirmation").value
+    if (email.length < 5){
+        alert("Email inválido")
+    }else if(username.length < 5){
+        alert("Username inválido")
+    }else if (passwordConfirmation != password){
+        alert("As senhas não são iguais!")
+    }else if (password.length < 5)
+        alert("Senha inválida!")
+    window.location.href = "./TelaPesquisa.html";
+}
