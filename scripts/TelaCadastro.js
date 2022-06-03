482e7a050106ffeb31be683d3d51042c4c3accad
KaydeSix <srsenhorial@gmail.com>
2022-06-03 20:39:06 -0300

    Validação top do cadastro

--- scripts/TelaCadastro.js
+++ scripts/TelaCadastro.js
@@ -1,8 +1,8 @@
 function ValidarCadastro() {
-    username = document.getElementById("username").value
-    email = document.getElementById("email").value
-    password = document.getElementById("password").value
-    passwordConfirmation = document.getElementById("passwordConfirmation").value
+    let username = document.getElementById("username").value
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+    let passwordConfirmation = document.getElementById("passwordConfirmation").value
     if (email.length < 5){
         alert("Email inválido")
     }else if(username.length < 5){
@@ -12,6 +12,29 @@ function ValidarCadastro() {
     }else if (password.length < 5){
         alert("Senha inválida!")
     }else{
-        window.location.href = "./TelaPesquisa.html";
+        let user = {username, email, password}
+        let users = JSON.parse(localStorage.getItem("users"))
+        //verifica se usuario ja existe
+        if (users != null) {
+            for (let cont = 0; cont < users.length; cont++) {
+                if (users[cont].email == email){
+                    alert("email já existe!")
+                    return
+                }
+            }
+        }
+        cadastra(users,user)
     }   
+}
+
+function cadastra(users,user) {
+
+    if (users == null) {
+        users = [user]
+    }
+    else{
+        users.push(user)
+    }
+    localStorage.setItem('users', JSON.stringify(users))
+    window.location.href = "./TelaLogin.html";
 }
