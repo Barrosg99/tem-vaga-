47aac05432b9108e5d4187be28037663169536b0
Gabriel Gonçalves <gbarros@multishopping.com.br>
2022-06-11 07:52:07 -0300

    fix cadastro/login js

--- scripts/TelaLogin.js
+++ scripts/TelaLogin.js
@@ -1,22 +1,21 @@
 function ValidarLogin() {
     login = document.getElementById("login").value
     senha = document.getElementById("senha").value
-    if (login.length >= 5 && senha.length >= 6){
-        window.location.href = "./TelaPesquisa.html";
-    }     
-    else{
-       alert("Um dos campos abaixo foi preenchido incorretamente")
-    }
 
-    let user = {email, password}
     let users = JSON.parse(localStorage.getItem("users"))
     if (users != null) {
+        let hasLoggedUser = false
         for (let cont = 0; cont < users.length; cont++) {
-            if (users[cont].email == email){
-                if (users[count].password == password){
+            if (users[cont].email == login){
+                if (users[cont].password == senha) {
+                    hasLoggedUser = true
                     window.location.href = "./TelaPesquisa.html"
+                    break
                 }
             }
         }
+        if (!hasLoggedUser) alert("Login ou senha, incorreto.")
+    } else {
+        alert("Você precisa se cadastrar antes")
     }
 }
--- scripts/TelaPesquisa.js
+++ scripts/TelaPesquisa.js
@@ -4,16 +4,29 @@ function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
+function myFunction2() {
+  console.log('aaa');
+  document.getElementById("myDropdow").classList.add("show");
+}
+
+function closeDropDownsByClassName(className) {
+  var dropdowns = document.getElementsByClassName(className);
+  var i;
+  for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
+    }
+  }
+}
+
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
   if (!event.target.matches('.dropbtn') && !event.target.matches('.dropdown-span') && !event.target.matches('.dropdown-svg')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+    closeDropDownsByClassName('dropdown-content')
+  }
+
+  if (!event.target.matches('.pesquisa')) {
+    closeDropDownsByClassName('dropdow-content')
   }
 }
